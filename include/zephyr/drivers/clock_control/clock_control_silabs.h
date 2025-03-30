602aaf70b455122cad7529af73c52f5b9e374637
janchri <jans.christoph@gmail.com>
2025-03-31 01:20:39 +0200

    boards: silabs: Initial efm32pg23/28 devKit support
    To support Pro DevKits of the EFM32 family, Kconfig, dts and dtsi files must be generated. The EFR family is used as a reference implementation.
    
    Signed-off-by: Christoph Jans <jans.christoph@gmail.com>

--- include/zephyr/drivers/clock_control/clock_control_silabs.h
+++ include/zephyr/drivers/clock_control/clock_control_silabs.h
@@ -20,9 +20,9 @@
 #elif defined(CONFIG_SOC_SERIES_EFR32BG27)
 #include <zephyr/dt-bindings/clock/silabs/xg27-clock.h>
 #elif defined(CONFIG_SOC_SERIES_EFM32PG23)
-#include <zephyr/dt-bindings/clock/silabs/pg23-clock.h>
+#include <zephyr/dt-bindings/clock/silabs/xg23-clock.h>
 #elif defined(CONFIG_SOC_SERIES_EFM32PG28)
-#include <zephyr/dt-bindings/clock/silabs/pg28-clock.h>
+#include <zephyr/dt-bindings/clock/silabs/xg28-clock.h>
 #elif defined(CONFIG_SOC_SERIES_EFR32BG29) || defined(CONFIG_SOC_SERIES_EFR32MG29)
 #include <zephyr/dt-bindings/clock/silabs/xg29-clock.h>
 #endif
